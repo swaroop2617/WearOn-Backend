5c6ec8d00951d144695fec0b19eb5df255907052
TSwaroop <swaroopthippapathi@gmail.com>
2026-06-07 18:35:45 +0530

    Change multer storage to memory storage

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -4,15 +4,7 @@ import { listProduct, addProduct, removeProduct, singleProduct } from "../contro
 import adminAuth from "../middleware/adminAuth.js";
 
 const productRouter = express.Router();
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads/");
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + "-" + file.originalname);
-  }
-});
-
+const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
 productRouter.post(
